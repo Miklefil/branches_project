d579d7bb1ccc779296165b5bebbaeb136e8c20f1
twist <twistysmig@mail.ru>
2023-02-01 17:18:47 +0300

    добавлена функция большой алфавит всей строки и описание.

--- str_func.py
+++ str_func.py
@@ -1,4 +1,13 @@
+#функция для всех больших букв
 def big_alfabet():
     a = str(input())
     return print(f'{a.upper()}')
+
 big_alfabet()
+
+#функция для всех первых больших букв
+def big_alfabet_first():
+    a = str(input())
+    return print(f'{a.title()}')
+
+big_alfabet_first()
